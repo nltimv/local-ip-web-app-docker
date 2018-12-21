d2d8452c32a996d8ca4505b58619e2300f1c69bb
Tim van Vugt <info@nltimv.com>
2018-12-22 00:25:37 +0100

    Add IP controller

---
+++ IPApplication1/Controllers/IPController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPApplication1.Controllers
+{
+    public class IPController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            
+            return Ok(HttpContext.Connection.LocalIpAddress.MapToIPv4().ToString());
+        }
+    }
+}
